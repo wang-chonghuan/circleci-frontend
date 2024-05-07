0c202fe438c828883f4b9340abec1206d9d2d011
wang-chonghuan <wangchonghuan@gmail.com>
2024-05-07 10:28:51 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:9001/test')
+      .then(response => {
+        setMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <p>Message from API: {message}</p>
+    </div>
+  );
+}
+
+export default App;
